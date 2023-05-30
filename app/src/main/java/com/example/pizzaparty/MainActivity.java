b0995a6d8bca2c6a09bcbae3618b0369e8e53efc
Jonathan Diaz <jrd17j@fsu.edu>
2023-05-30 17:57:53 -0400

    Added spinner

--- app/src/main/java/com/example/pizzaparty/MainActivity.java
+++ app/src/main/java/com/example/pizzaparty/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mNumAttendEditText;
     private TextView mNumPizzasTextView;
-    private RadioGroup mHowHungryRadioGroup;
+    private Spinner mHowHungrySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,24 @@ public class MainActivity extends AppCompatActivity {
         // Assign the widgets to fields
         mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
         mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
-        mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
+
+        mHowHungrySpinner = findViewById(R.id.hungry_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hunger_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mHowHungrySpinner.setAdapter(adapter);
+        mHowHungrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
 
         mNumAttendEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -57,15 +78,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Get hunger level selection
-        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        int checkedId = mHowHungrySpinner.getSelectedItemPosition();
         PizzaCalculator.HungerLevel hungerLevel = PizzaCalculator.HungerLevel.RAVENOUS;
-        if (checkedId == R.id.light_radio_button) {
+        if (checkedId == 0) {
             hungerLevel = PizzaCalculator.HungerLevel.LIGHT;
         }
-        else if (checkedId == R.id.medium_radio_button) {
+        else if (checkedId == 1) {
             hungerLevel = PizzaCalculator.HungerLevel.MEDIUM;
         }
 
+        else if (checkedId == 2){
+            hungerLevel = PizzaCalculator.HungerLevel.RAVENOUS;
+        }
         // Get the number of pizzas needed
         PizzaCalculator calc = new PizzaCalculator(numAttend, hungerLevel);
         int totalPizzas = calc.getTotalPizzas();
